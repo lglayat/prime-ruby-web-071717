bd39183d50f754b148618e9674aedac0d869fc08
Luke Glayat <glayatle@g.cofc.edu>
2017-07-08 02:20:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+def prime?(num)
+  if num < 0 || num == 0 || num == 1
+    return false
+  end
+
+  isPrime = true
+
+  for a in 2..(num - 1)
+   if (num % a) == 0
+    isPrime = false
+    return isPrime
+   end
+  end
+
+  return isPrime
+
+
+end
